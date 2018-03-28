b6c2ce80bca79c55eb740dbd265d38b1b7a0f0ff
Yi Chin <michaelexploreworld@gmail.com>
2018-03-28 21:52:40 +1100

    Deliver static data from the controller.

--- app_client/history/history.controller.js
+++ app_client/history/history.controller.js
@@ -4,6 +4,65 @@
     .controller('historyCtrl', historyCtrl);
 
   function historyCtrl () {
+    function listToMatrix(array, colNum) {
+      var matrix = [];
 
+      for (var arrayIndex = 0, row = -1; arrayIndex < array.length; arrayIndex++) {
+        var col = arrayIndex % colNum;
+        if (col == 0) {
+          matrix[++row] = [];
+        }
+        matrix[row][col] = array[arrayIndex];
+      }
+
+      return matrix;
+    }
+
+    var vm = this;
+    var historyList = [
+      {
+        time: '2018/03/06',
+        net: 1000,
+        sup: 1000,
+        netPlusSup: 1000,
+        gross: 1000,
+        grossPlusSup: 1000,
+        tax: 1000 },
+      {
+        time: '2018/03/06',
+        net: 1000,
+        sup: 1000,
+        netPlusSup: 1000,
+        gross: 1000,
+        grossPlusSup: 1000,
+        tax: 1000 },
+      {
+        time: '2018/03/06',
+        net: 1000,
+        sup: 1000,
+        netPlusSup: 1000,
+        gross: 1000,
+        grossPlusSup: 1000,
+        tax: 1000
+      }, {
+        time: '2018/03/06',
+        net: 1000,
+        sup: 1000,
+        netPlusSup: 1000,
+        gross: 1000,
+        grossPlusSup: 1000,
+        tax: 1000
+      }, {
+        time: '2018/03/06',
+        net: 1000,
+        sup: 1000,
+        netPlusSup: 1000,
+        gross: 1000,
+        grossPlusSup: 1000,
+        tax: 1000
+      }
+    ];
+
+    vm.historyMatrix = listToMatrix(historyList, 2);
   }
 })();
--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -5,9 +5,25 @@
 
   function homeCtrl () {
     var vm = this;
+
     vm.pageHeader = {
       title: 'Tax Estimation Calculator',
       strapline: 'Use this simple calculator to find out your estimated tax and net from a given income.'
+    };
+
+    vm.yearSeletorOptions = {
+      '2017 - 2018': { from: '2017', to: '2018' },
+      '2016 - 2017': { from: '2016', to: '2017' },
+      '2015 - 2016': { from: '2015', to: '2016' }
+    };
+
+    vm.annualResult = {
+      net: 1000,
+      sup: 1000,
+      netPlusSup: 1000,
+      gross: 1000,
+      grossPlusSup: 1000,
+      tax: 1000
     };
   }
 })();
